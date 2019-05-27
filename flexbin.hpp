dc9c6f56699243f3d7421aec8e41b444483e1d00
a.lipkin <a.lipkin@osnovalab.ru>
2019-05-27 15:53:28 +0300

    functions for reading required fields

--- flexbin.hpp
+++ flexbin.hpp
@@ -13,15 +13,19 @@
 
 #define FLEXBIN_SERIALIZE_FIXED(...) \
   auto flexbin_serialize_fixed() const { return std::forward_as_tuple(__VA_ARGS__); } \
+  auto flexbin_deserialize_fixed()     { return std::forward_as_tuple(__VA_ARGS__); } 
 
 #define FLEXBIN_SERIALIZE_REQUIRED(...) \
   auto flexbin_serialize_required() const { return std::forward_as_tuple(__VA_ARGS__); } \
+  auto flexbin_deserialize_required()     { return std::forward_as_tuple(__VA_ARGS__); } 
 
 #define FLEXBIN_SERIALIZE_OPTIONAL(...) \
-  auto flexbin_serialize_optional() const { return std::forward_as_tuple(__VA_ARGS__); } 
+  auto flexbin_serialize_optional() const { return std::forward_as_tuple(__VA_ARGS__); } \
+  auto flexbin_deserialize_optional()     { return std::forward_as_tuple(__VA_ARGS__); }
 
 #define FLEXBIN_SERIALIZE_SIMPLIFIED(...) \
-  auto flexbin_serialize_simplified() const { return std::forward_as_tuple(__VA_ARGS__); } 
+  auto flexbin_serialize_simplified() const { return std::forward_as_tuple(__VA_ARGS__); } \
+  auto flexbin_deserialize_simplified()     { return std::forward_as_tuple(__VA_ARGS__); } 
 
 namespace flexbin
 {
@@ -45,8 +49,8 @@ namespace flexbin
   {
     flexbin_reader<T> reader;
 
-    if(!reader.read(*this, obj))
-      throw( std::ios_base::failure("malformed object") );
+    if(!reader.read_required_fields(*this, obj))
+      throw( std::ios_base::failure("malformed object") ); // NO! set bad bit!
     return *this;
   }
 
--- flexbin_read.hpp
+++ flexbin_read.hpp
@@ -5,41 +5,54 @@
 
 namespace flexbin
 {
+  //////////////////////////
+  // Write strategies: fixes, optional, required, simplified
+  template<typename T>
+  inline bool read_required(ostream& ostr, uint8_t field_id, const T& value) {
+    //return field_writer<T>::pack(ostr, field_id, value);
+    return false;
+  };
+
+  ///////////////////////////
+
   template<typename T>
   struct flexbin_reader
   {
     uint8_t field_id = 0;
-
+    bool success_ = true;
     constexpr static bool required_fields_exists = (has_required_fields<T>::yes != 0);
     constexpr static bool optional_fields_exists = (has_optional_fields<T>::yes != 0);
     constexpr static bool fixed_fields_exists = (has_fixed_fields<T>::yes != 0);
     constexpr static bool simplified_fields_exists = (has_simplified_fields<T>::yes != 0);
 
    // Read fixed fields if exists
-    template<typename C = size_t>
+    template<typename C = bool>
     typename std::enable_if< fixed_fields_exists , C>::type
-    read_required_fields(istream& ostr, const T& obj) {
-/*      auto field_serializer_fixed = [this, &ostr](auto&&... args) { 
-          ( ( write_fixed(ostr, ++field_id,  args) ) , ... );
+    read_required_fields(istream& ostr, T& obj) {
+      auto field_deserializer_required = [this, &ostr](auto&&... args) { 
+          ( (success_ = success_ && read_required(ostr, ++field_id,  args) ) , ... );
         };
-      std::apply( field_serializer_fixed, obj.flexbin_serialize_fixed());*/
-      return 0;
+      std::apply(field_deserializer_required, obj.flexbin_deserialize_required());
+      return success_;
     }
 
    // Write fixed fields if not exists
-    template<typename C = size_t>
+    template<typename C = bool>
     typename std::enable_if< !fixed_fields_exists , C >::type
     read_required_fields( istream& ostr, const T& obj ) {
-      return 0;
+      return true;
     }
 
-    bool read( istream& ostr, const T& obj ) {
-      size_t nbytes_remain(0);
+    bool read_object_header( istream& ostr, const T& obj ) {
+/*      size_t nbytes_remain(0);
       ostr.read(reinterpret_cast<char *>(nbytes_remain), sizeof(nbytes_remain));
-      if(ostr.fail() || ostr.eof())
-        return 0;
+      if(ostr.fail() || ostr.eof())*/
+      return false;
     }  
 
+    bool read_object_endmarker(istream& ostr, const T& obj) {
+      return false;
+    }
   };
 
 }
